d3cf6d365c94274711c57a2db709136aeeca6269
leandrorochaadm <leandrorochaadm@gmail.com>
2020-12-11 09:54:10 -0400

    modelo usuario

---
+++ api/src/modelos/usuarios.go
@@ -0,0 +1,12 @@
+package modelos
+
+import "time"
+
+// Usuario representa o modelo de usuário
+type Usuario struct {
+	ID       uint64    `json:"id,omitempty"`
+	Nome     string    `json:"nome,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
+	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
