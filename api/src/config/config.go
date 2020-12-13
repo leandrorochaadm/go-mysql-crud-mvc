ff36eba36b021bf71cc32ffc70755e62a709c8a2
leandrorochaadm <leandrorochaadm@gmail.com>
2020-12-13 17:37:38 -0400

    assinando token

--- api/src/config/config.go
+++ api/src/config/config.go
@@ -15,6 +15,9 @@ var (
 
 	// Porta onde a API vai rodar
 	Porta = 0
+
+	// SecretKey é a chave que vai ser usada para assinar o token
+	SecretKey []byte
 )
 
 // Carregar vai inicializar variaveis de ambiente
@@ -34,5 +37,7 @@ func Carregar() {
 	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
 		os.Getenv("DB_USUARIO"), os.Getenv("DB_SENHA"), os.Getenv("DB_NOME"),
 	)
+
+	SecretKey = []byte(os.Getenv("SECRET_KEY"))
 
 }
